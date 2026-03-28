e0e4ed18624ebbe48ef3261c50da02ea22adecd9
Srujan <srujansmurty@gmail.com>
2026-03-28 11:20:11 +0530

    Set up and deploy 😄

--- Track-2/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -a
-source /c/Google-ADK-AI-Agents/Track-2/weather_decision_agent/.env
-set +a
-
-cd /c/Google-ADK-AI-Agents/Track-2
-
-uvx --from google-adk==1.27.2 adk deploy cloud_run --project=$PROJECT_ID --region=us-central1 --service_name=weather-decision-agent --with_ui ./weather_decision_agent -- --labels=dev-hackathon=use-adk --service-account=$SERVICE_ACCOUNT
